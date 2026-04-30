5d1d1161b227c3fb7ddc614efc24a1b5ed807108
Goddii <otienogodwin23@gmail.com>
2026-04-30 12:58:12 +0300

    style: add linear background style to file

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ function App() {
 
   return (
     <div className='app-container'>
+      <div className='clock-logo'>
+        <i class="bi bi-clock-fill"></i>
+      </div>
       <h1> Color Clock</h1>
       <p> The current time is: {new Date().toLocaleTimeString()} </p>
       <p> The current date is: {new Date().toLocaleDateString()}</p>
